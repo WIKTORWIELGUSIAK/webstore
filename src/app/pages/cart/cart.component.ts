f60066cf1dd9bb066f8f37f1464bffc8e6d6562c
WiktorWielgusiak <witia16@gmail.com>
2023-03-05 12:59:21 +0100

    test: test stripToken

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -47,7 +47,7 @@ export class CartComponent implements OnInit {
     private environment: EnvService
   ) {}
   ngOnInit(): void {
-    console.log(this.environment.testEnv);
+    console.log(this.environment.stripeToken);
     this.cartService.cart.subscribe((_cart: Cart) => {
       this.cart = _cart;
       this.dataSource = this.cart.items;
