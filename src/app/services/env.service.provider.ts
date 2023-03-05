734bd91d19a2ca25e01a8e11a0070eedc86f171b
WiktorWielgusiak <witia16@gmail.com>
2023-03-05 12:49:56 +0100

    test:test

--- src/app/services/env.service.provider.ts
+++ src/app/services/env.service.provider.ts
@@ -1,18 +1,17 @@
-import { EnvService } from './env.service'; // import environment service
+import { EnvService } from './env.service';
+
+interface MyWindow extends Window {
+  __env: Record<string, string>;
+}
 
 export const EnvServiceFactory = () => {
-  const env = new EnvService(); // Create env Instance
+  const env = new EnvService();
+  const browserWindow = window as unknown as MyWindow;
+  const browserWindowEnv = browserWindow.__env || {};
 
-  // Read environment variables from browser window
-  const browserWindow = window || {};
-  const browserWindowEnv = browserWindow['__env'] || {};
-
-  // Assign environment variables from browser window to env
-  // In the current implementation, properties from env.js overwrite defaults from the EnvService.
-  // If needed, a deep merge can be performed here to merge properties instead of overwriting them.
   for (const key in browserWindowEnv) {
     if (browserWindowEnv.hasOwnProperty(key)) {
-      env[key] = window['__env'][key];
+      (env as Record<string, any>)[key] = browserWindowEnv[key];
     }
   }
 
